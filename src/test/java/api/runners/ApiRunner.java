209eef2e8e0818edc1dfb2e8cd03019b370f655c
Ardella Putra <ardellaputra@Ardellas-MacBook-Pro-2.local>
2026-06-18 16:15:43 +0700

    complete web and api automation framework

--- src/test/java/api/runners/ApiRunner.java
+++ src/test/java/api/runners/ApiRunner.java
@@ -1,7 +1,26 @@
+//package api.runners;
+//
+//import io.cucumber.testng.AbstractTestNGCucumberTests;
+//import io.cucumber.testng.CucumberOptions;
+//
+//@CucumberOptions(
+//        features = "src/test/resources/features/api",
+//        glue = {"api.stepdefinitions"},
+//        plugin = {
+//                "pretty",
+//                "html:reports/api-report.html",
+//                "json:reports/api-report.json"
+//        }
+//)
+//public class ApiRunner
+//        extends AbstractTestNGCucumberTests {
+//}
+
 package api.runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "src/test/resources/features/api",
@@ -12,6 +31,11 @@ import io.cucumber.testng.CucumberOptions;
                 "json:reports/api-report.json"
         }
 )
-public class ApiRunner
-        extends AbstractTestNGCucumberTests {
+public class ApiRunner extends AbstractTestNGCucumberTests {
+
+        @Override
+        @DataProvider(parallel = false)
+        public Object[][] scenarios() {
+                return super.scenarios();
+        }
 }
--- src/test/java/web/runners/WebRunner.java
+++ src/test/java/web/runners/WebRunner.java
@@ -1,23 +1,50 @@
+//package web.runners;
+//
+//import io.cucumber.testng.AbstractTestNGCucumberTests;
+//import io.cucumber.testng.CucumberOptions;
+//
+//@CucumberOptions(
+//        features =
+//                "src/test/resources/features/web",
+//
+//        glue = {
+//                "web.stepdefinitions",
+//                "web.hooks"
+//        },
+//
+//        plugin = {
+//                "pretty",
+//                "html:reports/web-report.html",
+//                "json:reports/web-report.json"
+//        }
+//)
+//public class WebRunner
+//        extends AbstractTestNGCucumberTests {
+//}
+
 package web.runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
-        features =
-                "src/test/resources/features/web",
-
+        features = "src/test/resources/features/web",
         glue = {
                 "web.stepdefinitions",
                 "web.hooks"
         },
-
         plugin = {
                 "pretty",
                 "html:reports/web-report.html",
                 "json:reports/web-report.json"
         }
 )
-public class WebRunner
-        extends AbstractTestNGCucumberTests {
+public class WebRunner extends AbstractTestNGCucumberTests {
+
+        @Override
+        @DataProvider(parallel = false)
+        public Object[][] scenarios() {
+                return super.scenarios();
+        }
 }
