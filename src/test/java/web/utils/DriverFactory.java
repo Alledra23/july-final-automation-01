7496a024d79694cfe52a3fbeb4db87569a3e0ffb
Ardella Putra <ardellaputra@Ardellas-MacBook-Pro-2.local>
2026-06-26 13:34:56 +0700

    fix add headless Chrome for GitHub

--- src/test/java/web/utils/DriverFactory.java
+++ src/test/java/web/utils/DriverFactory.java
@@ -2,25 +2,32 @@ package web.utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
     private static WebDriver driver;
 
     public static void setupDriver() {
+        ChromeOptions options = new ChromeOptions();
 
-        driver = new ChromeDriver();
+        // headless mode untuk GitHub Actions (tidak ada display)
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
     }
 
     public static WebDriver getDriver() {
-
         return driver;
     }
 
     public static void quitDriver() {
-
-        if(driver != null) {
+        if (driver != null) {
             driver.quit();
         }
     }
